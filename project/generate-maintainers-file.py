22deb3c3dea87ebd938b6aa78912f3d16c0c55cb
Daniel Holbach <daniel@weave.works>
2021-12-01 15:32:40 +0100

    allow specifiying other output files, which will help us generate other MAINTAINERs files too
    Signed-off-by: Daniel Holbach <daniel@weave.works>

--- project/generate-maintainers-file.py
+++ project/generate-maintainers-file.py
@@ -9,7 +9,6 @@ import yaml
 YAML_PATH = os.path.dirname(__file__)
 MAINTAINER_INFO_FN = os.path.join(YAML_PATH, 'flux-project-maintainers.yaml')
 ROLODEX_FN = os.path.join(YAML_PATH, 'rolodex.yaml')
-MAINTAINERS_FN = os.path.join(YAML_PATH, '../MAINTAINERS')
 
 INFO_HEADER = '''The maintainers are generally available in Slack at
 https://cloud-native.slack.com in #flux (https://cloud-native.slack.com/messages/CLAJ40HV3)
@@ -47,6 +46,7 @@ class ProjectData():
 def main():
     parser = argparse.ArgumentParser(description='Print MAINTAINERS file for given repository.')
     parser.add_argument('repository', type=str, help='repository name')
+    parser.add_argument('-o', '--file', type=str, help='MAINTAINERS file location')
     args = parser.parse_args()
 
     project = ProjectData()
@@ -54,7 +54,9 @@ def main():
     if not text:
         sys.exit(0)
 
-    open(MAINTAINERS_FN, 'w').write(text)
+    if not args.file:
+        args.file = os.path.join(YAML_PATH, '../MAINTAINERS')
+    open(args.file, 'w').write(text)
 
 
 if __name__ == '__main__':
